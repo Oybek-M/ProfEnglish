ceaa765fc87c65ba1701417f5c85e151165c1a84
Oybek-M <muxtaraliyevoybek@gmail.com>
2026-07-05 18:13:39 +0500

    fix: translate remaining English pricing copy to Uzbek

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -443,19 +443,19 @@ export default function LandingPage(): JSX.Element {
                   <svg className="w-5 h-5 text-indigo-600 flex-shrink-0 mt-0.5" fill="currentColor" viewBox="0 0 20 20">
                     <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
                   </svg>
-                  <span className="text-gray-700">Unlimited analytics</span>
+                  <span className="text-gray-700">Cheksiz statistika va tahlil</span>
                 </div>
                 <div className="flex items-start space-x-3">
                   <svg className="w-5 h-5 text-indigo-600 flex-shrink-0 mt-0.5" fill="currentColor" viewBox="0 0 20 20">
                     <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
                   </svg>
-                  <span className="text-gray-700">Priority support</span>
+                  <span className="text-gray-700">Ustuvor qo'llab-quvvatlash</span>
                 </div>
                 <div className="flex items-start space-x-3">
                   <svg className="w-5 h-5 text-indigo-600 flex-shrink-0 mt-0.5" fill="currentColor" viewBox="0 0 20 20">
                     <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
                   </svg>
-                  <span className="text-gray-700">Custom lessons</span>
+                  <span className="text-gray-700">Maxsus moslashtirilgan darslar</span>
                 </div>
               </div>
 
